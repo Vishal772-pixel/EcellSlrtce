f1c3e4b08ad9f9790394cbe9669c0637dd6b39f6
Vishal772-pixel <fantasydark001@gmail.com>
2024-12-23 15:58:38 +0530

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -14,7 +14,7 @@ export const HeroSection = () => {
           <div className='absolute inset-0 bg-black opacity-50'></div>
           <div className='absolute inset-0 font-bold max-md:text-6xl text-8xl text-white h-screen flex justify-center items-center'>
              <div><div className='text text-center mb-4'>E-CELL</div>
-              <div className='text text-center flex max-md:flex-col'><div className='mx-4'>Shree L.R tiwari </div><div>College Of Engineering</div></div>
+              <div className='text text-center flex max-md:flex-col'><div className='mx-4'>Shree L.R Tiwari </div><div>College Of Engineering</div></div>
               <div 
               className='scrolldown absolute cursor-pointer left-1/2 bottom-0 w-[40px] h-[40px] translate-y-[-80px] translate-x-[-50%] rotate-45'
               onClick={scroll}
